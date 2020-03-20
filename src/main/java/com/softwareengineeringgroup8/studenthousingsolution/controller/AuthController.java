82c705b992393a5454c3265f29592d97e5f7075f
mgiannella <michael.giannella@gmail.com>
2020-03-20 13:18:15 -0400

    Fixed CORS

--- src/main/java/com/softwareengineeringgroup8/studenthousingsolution/controller/AuthController.java
+++ src/main/java/com/softwareengineeringgroup8/studenthousingsolution/controller/AuthController.java
@@ -14,7 +14,6 @@ import com.softwareengineeringgroup8.studenthousingsolution.config.JwtToken;
 import org.springframework.security.authentication.AuthenticationManager;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/auth")
 public class AuthController {
 
