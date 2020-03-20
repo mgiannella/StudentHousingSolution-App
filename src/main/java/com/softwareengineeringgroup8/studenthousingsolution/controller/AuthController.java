cceef444c8584177b0cfe170f80de7858dba144c
mgiannella <michael.giannella@gmail.com>
2020-03-20 13:21:55 -0400

    Added CORS back

--- src/main/java/com/softwareengineeringgroup8/studenthousingsolution/controller/AuthController.java
+++ src/main/java/com/softwareengineeringgroup8/studenthousingsolution/controller/AuthController.java
@@ -14,6 +14,7 @@ import com.softwareengineeringgroup8.studenthousingsolution.config.JwtToken;
 import org.springframework.security.authentication.AuthenticationManager;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/auth")
 public class AuthController {
 
