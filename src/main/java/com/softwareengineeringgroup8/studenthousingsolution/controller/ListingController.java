07978a5472e663b34d52078718007c4201b5b5f5
Rishi Shah <RishiShah@MacBook-Pro-3.local>
2020-03-21 14:29:46 -0400

    Backend basically done

--- src/main/java/com/softwareengineeringgroup8/studenthousingsolution/controller/ListingController.java
+++ src/main/java/com/softwareengineeringgroup8/studenthousingsolution/controller/ListingController.java
@@ -3,16 +3,14 @@ package com.softwareengineeringgroup8.studenthousingsolution.controller;
 
 import com.softwareengineeringgroup8.studenthousingsolution.model.ListingRequest;
 
-import com.softwareengineeringgroup8.studenthousingsolution.model.RegisterRequest;
-import com.softwareengineeringgroup8.studenthousingsolution.repository.ListingRepository;
-import com.softwareengineeringgroup8.studenthousingsolution.service.ListingService;
+//import com.softwareengineeringgroup8.studenthousingsolution.repository.ListingRepository;
+//import com.softwareengineeringgroup8.studenthousingsolution.service.ListingService;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import javassist.NotFoundException;
 
-import org.springframework.ui.Model;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
@@ -25,21 +23,39 @@ import java.util.Map;
 @RequestMapping("/listing")
 public class ListingController{
 
-    @Autowired
-    private ListingService service;
+    //@Autowired
+   // private ListingService service;
 
 
 
     @PostMapping("/create")
     @ApiOperation(value="Create Listing",notes="Create new property listing and store the data in the database.")
-    public Boolean newListing(@RequestBody ListingRequest newListing) throws NoSuchAlgorithmException {
-        try {
+    public Boolean newListing(@RequestBody ListingRequest request) throws NoSuchAlgorithmException {
+        String title = request.getTitle();
+        String address = request.getAddress();
+        String city = request.getCity();
+        String state = request.getState();
+        String zipCode = request.getZipCode();
+        double price = request.getPrice();
+        int numBedrooms = request.getNumBedrooms();
+        int numBathrooms = request.getNumBathrooms();
+        boolean hasAC=request.isHasAC();
+        int parkingspots = request.getParkingspots();
+        boolean hasLaundry=request.isHasLaundry();
+        boolean allowPets = request.isAllowPets();
+        boolean allowSmoking = request.isAllowSmoking();
+
+        System.out.println(zipCode);
+        return false;
+       /* try {
             service.create(newListing);
             return true;
         }catch(Error e){
             System.out.println(e);
             return false;
         }
+
+        */
     }
 
 }
