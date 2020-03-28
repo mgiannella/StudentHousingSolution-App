a57abca94552207edf4b9ee307346b03e0e938ab
Rishab <rishabravikumar@yahoo.com>
2020-03-28 18:36:43 -0400

    Pesky Bug fix

--- src/main/java/com/softwareengineeringgroup8/studenthousingsolution/repository/AmenitiesRepository.java
+++ src/main/java/com/softwareengineeringgroup8/studenthousingsolution/repository/AmenitiesRepository.java
@@ -18,16 +18,16 @@ public interface AmenitiesRepository extends JpaRepository<Amenities,Integer> {
             + "and (a.sleeps>= :#{#filter.sleeps.min} and a.sleeps<= :#{#filter.sleeps.max}) "
             + "and (a.numBedrooms>= :#{#filter.bedrooms.min} and a.numBedrooms<= :#{#filter.bedrooms.max}) "
             + "and (a.numBathrooms>= :#{#filter.bathrooms.min} and a.price<= :#{#filter.bathrooms.max}) "
-            + "and a.hasAC >= :#{#filter.hasAC} "
-            + "and a.laundry >= :#{#filter.laundry} "
-            + "and a.parkingSpots >= :#{#filter.parking} "
-            + "and a.petsAllowed >= :#{#filter.petsAllowed} "
-            + "and a.smokingAllowed >= :#{#filter.smokingAllowed} "
-            + "and a.waterUtility >= :#{#filter.waterUtility} "
-            + "and a.gasElectricUtil >= :#{#filter.gasElectricUtil} "
-            + "and a.isFurnished >= :#{#filter.isFurnished} "
-            + "and a.hasAppliances >= :#{#filter.hasAppliances} "
-            + "and a.isTrashPickedUpl >= :#{#filter.isTrashPickedUpl} "
-            + "and a.hasHeat >= :#{#filter.hasHeat}")
+            + "and (a.hasAC >= :#{#filter.hasAC}) "
+            + "and (a.hasLaundry >= :#{#filter.hasLaundry}) "
+            + "and (a.parkingSpots >= :#{#filter.parking}) "
+            + "and (a.petsAllowed >= :#{#filter.petsAllowed}) "
+            + "and (a.smokingAllowed >= :#{#filter.smokingAllowed}) "
+            + "and (a.waterUtility >= :#{#filter.waterUtility}) "
+            + "and (a.gasElectricUtil >= :#{#filter.gasElectricUtil}) "
+            + "and (a.isFurnished >= :#{#filter.isFurnished}) "
+            + "and (a.hasAppliances >= :#{#filter.hasAppliances}) "
+            + "and (a.isTrashPickedUpl >= :#{#filter.isTrashPickedUpl}) "
+            + "and (a.hasHeat >= :#{#filter.hasHeat})")
     List<Amenities> filterSearch(@Param("filter")SearchFilterRequest values);
 }
