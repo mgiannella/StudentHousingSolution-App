a13f804fb7dc5af61edeed4d10205c292376c874
mgiannella <michael.giannella@gmail.com>
2020-04-22 00:26:42 -0400

    New method in TG

--- src/main/java/com/softwareengineeringgroup8/studenthousingsolution/repository/TenantGroupsRepository.java
+++ src/main/java/com/softwareengineeringgroup8/studenthousingsolution/repository/TenantGroupsRepository.java
@@ -1,6 +1,7 @@
 package com.softwareengineeringgroup8.studenthousingsolution.repository;
 
 import com.softwareengineeringgroup8.studenthousingsolution.model.TenantGroups;
+import com.softwareengineeringgroup8.studenthousingsolution.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,4 +9,8 @@ import java.util.List;
 
 public interface TenantGroupsRepository extends JpaRepository<TenantGroups, Integer> {
     TenantGroups findById(int id);
+
+    @Query("SELECT tg FROM TenantGroups tg WHERE tg.leadTenant = ?1")
+    List<TenantGroups> findGroupByLeadMember(User user);
+
 }
