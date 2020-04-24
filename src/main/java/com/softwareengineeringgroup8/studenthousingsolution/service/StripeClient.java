f1584363d7fbbc1078479fba99df8b9f822c5f84
Srinivas <sn609@scarletmail.rutgers.edu>
2020-04-23 23:40:59 -0400

    bank changes

--- src/main/java/com/softwareengineeringgroup8/studenthousingsolution/service/StripeClient.java
+++ src/main/java/com/softwareengineeringgroup8/studenthousingsolution/service/StripeClient.java
@@ -76,66 +76,73 @@ public class StripeClient {
     public String createLandlordAcct(String email, User landlord, String account_holder_name, String routing_number, String account_number ) throws StripeException {
 
         String accountId = null;
+        String bankId= null;
+
         //creating a landlord account
+        ArrayList<Object> requestedCapabilities =new ArrayList<>();
+        requestedCapabilities.add("card_payments");
+        requestedCapabilities.add("transfers");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("type", "custom");
+        params.put("country", "US");
+        params.put("email", email);
+        params.put("requested_capabilities", Arrays.asList("card_payments", "transfers"));
+
+        Account account = Account.create(params);
+        accountId=account.getId();
+
+
+        //Accepting terms of agreement
+        Account acct = Account.retrieve(accountId);
+
+        Map<String, Object> tosAcceptanceParams = new HashMap<>();
+        tosAcceptanceParams.put("date", (long) System.currentTimeMillis() / 1000L);
+        tosAcceptanceParams.put("ip", "137.36.251.66"); // We're not using a proxy
+
+        Map<String, Object> parameter = new HashMap<>();
+        parameter.put("tos_acceptance", tosAcceptanceParams);
+
+        acct.update(parameter);
+
+        //Connects a bank account
+        Account a= Account.retrieve(accountId);
+        Map<String, Object> tokenParam = new HashMap<>();
+        Map <String, Object> bankAccountParam=new HashMap<>();
+
+        bankAccountParam.put("country", "US");
+        bankAccountParam.put("currency", "usd");
+        bankAccountParam.put("account_holder_name", account_holder_name);
+        bankAccountParam.put("account_holder_type", "individual");
+        bankAccountParam.put("routing_number", routing_number );
+        bankAccountParam.put("account_number", account_number);
+        tokenParam.put("bank_account", bankAccountParam);
+        Token token = Token.create(tokenParam);
+
+        String bankToken=token.getId();
+
+        Map<String, Object> acctParams = new HashMap<>();
+        acctParams.put("external_account", bankToken);
+
+        BankAccount bankAccount = (BankAccount) a.getExternalAccounts().create(acctParams);
+
+
         try {
 
-            // Creates the account
-            ArrayList<Object> requestedCapabilities =new ArrayList<>();
-            requestedCapabilities.add("card_payments");
-            requestedCapabilities.add("transfers");
 
-            Map<String, Object> params = new HashMap<>();
-            params.put("type", "custom");
-            params.put("country", "US");
-            params.put("email", email);
-            params.put("requested_capabilities", Arrays.asList("card_payments", "transfers"));
-
-            Account account = Account.create(params);
-            accountId=account.getId();
-
-
-            //Accepting terms of agreement
-            Account acct = Account.retrieve(accountId);
-
-            Map<String, Object> tosAcceptanceParams = new HashMap<>();
-            tosAcceptanceParams.put("date", (long) System.currentTimeMillis() / 1000L);
-            tosAcceptanceParams.put("ip", "137.36.251.66"); // We're not using a proxy
-
-            Map<String, Object> parameter = new HashMap<>();
-            parameter.put("tos_acceptance", tosAcceptanceParams);
-
-            acct.update(parameter);
-
-            //Connects a bank account
-            Account a= Account.retrieve(accountId);
-            Map<String, Object> tokenParam = new HashMap<>();
-            Map <String, Object> bankAccountParam=new HashMap<>();
-
-            bankAccountParam.put("country", "US");
-            bankAccountParam.put("currency", "usd");
-            bankAccountParam.put("account_holder_name", account_holder_name);
-            bankAccountParam.put("account_holder_type", "individual");
-            bankAccountParam.put("routing_number", routing_number );
-            bankAccountParam.put("account_number", account_number);
-            tokenParam.put("bank_account", bankAccountParam);
-            Token token = Token.create(tokenParam);
-
-            String bankToken=token.getId();
-
-            Map<String, Object> acctParams = new HashMap<>();
-            acctParams.put("external_account", bankToken);
-
-            BankAccount bankAccount = (BankAccount) a.getExternalAccounts().create(acctParams);
-
+            bankId=bankAccount.getId();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
 
 
-        LandlordAccounts landlordAccounts= new LandlordAccounts(landlord, accountId);
-        landlordAccountsRepository.save(landlordAccounts);
-
+        if(accountId!=null|| bankId!=null) {
+            LandlordAccounts landlordAccounts = new LandlordAccounts(landlord, accountId);
+            landlordAccountsRepository.save(landlordAccounts);
+        }else{
+            return null;
+        }
         return accountId;
     }
 
