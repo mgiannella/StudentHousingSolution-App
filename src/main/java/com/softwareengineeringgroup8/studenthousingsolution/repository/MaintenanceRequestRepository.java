44e04ec3427666838893528d518c542abf6c6fa8
snehshah <snehs117@gmail.com>
2020-05-07 12:29:22 -0400

    added name on top

--- src/main/java/com/softwareengineeringgroup8/studenthousingsolution/repository/MaintenanceRequestRepository.java
+++ src/main/java/com/softwareengineeringgroup8/studenthousingsolution/repository/MaintenanceRequestRepository.java
@@ -1,10 +1,14 @@
+/*
+written by: Sneh Shah
+tested by: Sneh Shah
+debugged by: Sneh Shah
+*/
 package com.softwareengineeringgroup8.studenthousingsolution.repository;
 
 import com.softwareengineeringgroup8.studenthousingsolution.model.MaintenanceRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 import com.softwareengineeringgroup8.studenthousingsolution.model.Properties;
 
